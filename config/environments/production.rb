58db620f171d149ff2cb25d57c555346d2ea5e90
Daniela Stoyanov <dstoianova75@yahoo.com>
2015-02-26 11:25:02 -0500

    Implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Requered for Heroku
+  # Set your actual host name
+  config.action_mailer.default_url_options = { host: 'stoyanov.herokuapp.com', port: 3000 }
 end
